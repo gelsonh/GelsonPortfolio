ec0f91fa45823d45388d534b6cb52e1e17b02c66
Gelson Hernandez <gelsonpf49@hotmail.com>
2023-10-23 11:24:38 -0400

    Added Logo & URL: Dev Diary

--- js/blogApi.js
+++ js/blogApi.js
@@ -1,61 +1,72 @@
-function fetchBlogData() {
-  const baseUrl = "https://thewritinglife-production.up.railway.app/";
+document.addEventListener("DOMContentLoaded", function () {
+  function fetchBlogData() {
+    const baseUrl = "https://devdiary-production.up.railway.app";
 
-  // Fetch a specific blog post by ID (in this case, ID 3)
-  fetch(`${baseUrl}/api/BlogPosts/3`)
-    .then((response) => response.json())
-    .then(function (data) {
-      displayBlogData(data, baseUrl);
-    })
-    .catch((error) => console.error(error));
-}
+    fetch(`${baseUrl}/api/BlogPosts/1`)
+      .then((response) => response.json())
+      .then(function (data) {
+        displayBlogData(data, baseUrl);
+      });
+  }
 
-function displayBlogData(blogPosts, baseUrl) {
-  let template = document.getElementById("blog-template");
-  let blogSection = document.getElementById("blogs");
+  function displayBlogData(blogPosts, baseUrl) {
+    let template = document.getElementById("blog-template");
+    let blogSection = document.getElementById("blogs");
 
-  // Clear the existing content in the blog section
-  blogSection.innerHTML = "";
+    blogPosts.forEach((blogPost) => {
+      // Clone the template node to create a new blog post element
+      const blogPostCard = document.importNode(template.content, true);
 
-  blogPosts.forEach((blogPost) => {
-    // Clone the template node to create a new blog post element
-    const blogPostCard = document.importNode(template.content, true);
+      // Update the blog post content using data from the API
+      // Set the title
+      const blogTitle = blogPostCard.querySelector('[data-blog="title"]');
+      blogTitle.textContent = blogPost.title;
 
-    // Update the blog post content using data from the API
-    // Set the title
-    const blogTitle = blogPostCard.querySelector('[data-blog="title"]');
-    blogTitle.textContent = blogPost.title;
+      // Set the image
+      const blogImage = blogPostCard.querySelector(".blog-image");
+      blogImage.src = `${baseUrl}/content/${blogPost.imageFile}`; // Update with the correct field
+      blogImage.alt = blogPost.title;
 
-    // Set the image
-    const blogImage = blogPostCard.querySelector('[data-blog="image"]');
-    blogImage.src = `${baseUrl}/content/${blogPost.slug}`;
-    blogImage.alt = blogPost.title;
+      // Set the content (abstract in this case)
+      const blogContent = blogPostCard.querySelector('[data-blog="abstract"]'); // Update to match the field name
+      blogContent.textContent = blogPost.abstract; // Update to match the field name
 
-    // Set the content
-    const blogContent = blogPostCard.querySelector('[data-blog="content"]');
-    blogContent.innerHTML = blogPost.abstract;
+      // Set the category
+      const blogCategory = blogPostCard.querySelector('[data-blog="category"]');
+      blogCategory.textContent = blogPost.category; // Update to match the field name
 
-    // Set the read more link
-    const blogLink = blogPostCard.querySelector('[data-blog="readMoreLink"]');
-    blogLink.href = `${baseUrl}/content/${blogPost.slug}`;
+      // Set the content
+      const blogFullContent = blogPostCard.querySelector(
+        '[data-blog="content"]'
+      );
+      blogFullContent.textContent = blogPost.content;
 
-    // Set the published date
-    const blogPubDate = blogPostCard.querySelector(
-      '[data-blog="publishedDate"]'
-    );
-    const createdDate = new Date(blogPost.created);
-    const currentDate = new Date();
+      // Set the tags
+      const blogTags = blogPostCard.querySelector('[data-blog="tags"]');
+      blogTags.textContent = blogPost.tags; // Update to match the field name
 
-    // Calculate the time difference
-    const timeDifference = currentDate - createdDate;
-    const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      // Set the published date
+      const blogPubDate = blogPostCard.querySelector(
+        '[data-blog="publishedDate"]'
+      );
+      const createdDate = new Date(blogPost.created);
+      const currentDate = new Date();
+      const timeDifference = currentDate - createdDate;
+      const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      blogPubDate.textContent =
+        daysAgo === 1
+          ? `Published ${daysAgo} day ago`
+          : `Published ${daysAgo} days ago`;
 
-    blogPubDate.textContent =
-      daysAgo === 1
-        ? `Published ${daysAgo} day ago`
-        : `Published ${daysAgo} days ago`;
+      // Set the read more link
+      const blogLink = blogPostCard.querySelector('[data-blog="readMoreLink"]');
+      blogLink.href = `${baseUrl}/content/${blogPost.slug}`;
 
-    // Append the blog post element to the blog section
-    blogSection.appendChild(blogPostCard);
-  });
-}
+      // Append the blog post element to the blog section
+      blogSection.appendChild(blogPostCard);
+    });
+  }
+
+  // Llamar a la función para cargar los datos
+  fetchBlogData();
+});
