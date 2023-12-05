2f73540ce5351f4b70d97cf7668362ed61e8fcd1
Gelson Hernandez <gelsonpf49@hotmail.com>
2023-12-05 07:45:48 -0500

    Added Dark Mode

--- js/site.js
+++ js/site.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const switchInput = document.querySelector(".switch__input");
 
+  switchInput.addEventListener("change", function () {
+    if (switchInput.checked) {
+      // Cambiar a tema dark
+      document.body.setAttribute("data-gh-theme", "dark");
+    } else {
+      // Cambiar a tema light
+      document.body.setAttribute("data-gh-theme", "light");
+    }
+  });
+});
