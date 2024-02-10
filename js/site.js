84b3ac56d84e49f1676b4470bab453023c5c864e
Gelson Hernandez <gelsonpf49@hotmail.com>
2024-02-10 03:06:17 -0500

    Added New Switch DarkMode & New Socialicons

--- js/site.js
+++ js/site.js
@@ -1,5 +1,19 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const switchInput = document.querySelector(".switch__input");
+
+//   switchInput.addEventListener("change", function () {
+//     if (switchInput.checked) {
+//       // Cambiar a tema dark
+//       document.body.setAttribute("data-gh-theme", "dark");
+//     } else {
+//       // Cambiar a tema light
+//       document.body.setAttribute("data-gh-theme", "light");
+//     }
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
-  const switchInput = document.querySelector(".switch__input");
+  const switchInput = document.querySelector(".bb8-toggle__checkbox");
 
   switchInput.addEventListener("change", function () {
     if (switchInput.checked) {
