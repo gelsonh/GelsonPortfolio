8cf936cc0342d4788edc8c25e5ffc2be36c21c39
Gelson Hernandez <gelsonpf49@hotmail.com>
2024-07-01 13:20:58 -0400

    Added Comments in blogApi.js

--- js/blogApi.js
+++ js/blogApi.js
@@ -1,66 +1,73 @@
+// Fetch and Display Latest Blog Posts
+
 document.addEventListener("DOMContentLoaded", function () {
+  // Function to fetch blog data from the API
   function fetchBlogData() {
     const baseUrl = "https://devdiary-production.up.railway.app";
 
+    // Fetching the latest 3 blog posts from the API
     fetch(`${baseUrl}/api/BlogPosts/3`)
       .then((response) => response.json())
-      // This line of code converts the body of the Fetch response into a JavaScript object using the JSON format.
-      // This makes it easier to handle and use the data in the code, as JSON is easily parseable in JavaScript.
+      // Convert the response body to a javascript object using JSON
       .then(function (data) {
-        
         // Sort the blog posts by creation date in descending order
         const sortedBlogPosts = data.sort(
           (a, b) => new Date(b.created) - new Date(a.created)
         );
 
-        // Get the first three blog posts
+        // Get the first three blog posts from the sorted list
         const latestBlogPosts = sortedBlogPosts.slice(0, 3);
 
+        // Display the blog posts on the page
         displayBlogData(latestBlogPosts, baseUrl);
       });
   }
 
+  // Function to display the fetched blog data on the page
   function displayBlogData(blogPosts, baseUrl) {
-    let template = document.getElementById("blog-template");
-    let blogSection = document.getElementById("blogs");
+    let template = document.getElementById("blog-template"); // Get the blog post template
+    let blogSection = document.getElementById("blogs"); // Get the blog section where post will be displayed
 
     blogPosts.forEach((blogPost) => {
       // Clone the template node to create a new blog post element
       const blogPostCard = document.importNode(template.content, true);
 
       // Update the blog post content using data from the API
+
       // Set the title
       const blogTitle = blogPostCard.querySelector('[data-blog="title"]');
       blogTitle.textContent = blogPost.title;
 
       // Set the image
       const blogImage = blogPostCard.querySelector(".blog-image");
-      blogImage.src = blogPost.imageFileString; // Update with the correct field
-      let blogDate = new Date(blogPost.created); // 2009-11-10
+      blogImage.src = blogPost.imageFileString;
+
+      // Format and set the blog post creation date
+      let blogDate = new Date(blogPost.created);
       let month = blogDate.toLocaleString("default", {month: "long"});
       let day = blogDate.getDate();
 
-      //add day
+      // Set the day
       let blogDayDiv = blogPostCard.querySelector(".blog-day");
       blogDayDiv.innerHTML = day;
 
-      //add month
+      // Set the month
       let blogMonthDiv = blogPostCard.querySelector(".blog-month");
-
       blogMonthDiv.innerHTML = month;
 
       // Set the content (abstract in this case)
-      const blogContent = blogPostCard.querySelector('[data-blog="abstract"]'); // Update to match the field name
-      blogContent.textContent = blogPost.abstract; // Update to match the field name
+      const blogContent = blogPostCard.querySelector('[data-blog="abstract"]');
+      blogContent.textContent = blogPost.abstract;
 
-      // Set the published date
+      // Set the published date in a relative format
       const blogPubDate = blogPostCard.querySelector(
         '[data-blog="publishedDate"]'
       );
+
       const createdDate = new Date(blogPost.created);
       const currentDate = new Date();
-      const timeDifference = currentDate - createdDate;
-      const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      const timeDifference = currentDate - createdDate; // Calculate the time difference
+      const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24)); // Convert time difference to days
       blogPubDate.textContent =
         daysAgo === 1
           ? `Published ${daysAgo} day ago`
@@ -71,6 +78,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-  // Llamar a la función para cargar los datos
+  // Call the function to load the blog data when the page content is loaded
   fetchBlogData();
 });
