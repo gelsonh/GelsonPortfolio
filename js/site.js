a3757d6c26f9a3fd03522191315f347779d7bc75
Gelson Hernandez <gelsonpf49@hotmail.com>
2024-07-01 14:59:51 -0400

    Deleted & Added Comments in site.js

--- js/site.js
+++ js/site.js
@@ -1,26 +1,14 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const switchInput = document.querySelector(".switch__input");
-
-//   switchInput.addEventListener("change", function () {
-//     if (switchInput.checked) {
-//       // Cambiar a tema dark
-//       document.body.setAttribute("data-gh-theme", "dark");
-//     } else {
-//       // Cambiar a tema light
-//       document.body.setAttribute("data-gh-theme", "light");
-//     }
-//   });
-// });
+// Dark Mode Function
 
 document.addEventListener("DOMContentLoaded", function () {
   const switchInput = document.querySelector(".bb8-toggle__checkbox");
 
   switchInput.addEventListener("change", function () {
     if (switchInput.checked) {
-      // Cambiar a tema dark
+      // Switch to dark theme
       document.body.setAttribute("data-gh-theme", "dark");
     } else {
-      // Cambiar a tema light
+      // Switch to light theme
       document.body.setAttribute("data-gh-theme", "light");
     }
   });
