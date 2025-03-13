144f1f64d2b34fcf26efedd8ab5a727d9c33ec90
Gelson Hernandez <gelsonpf49@hotmail.com>
2025-03-13 15:10:24 -0400

    Add new Project and Modal. Update Dark-Mode JavaScript code

--- js/site.js
+++ js/site.js
@@ -4,37 +4,36 @@ document.addEventListener("DOMContentLoaded", function () {
   // Function to apply the theme
   function applyTheme(theme) {
     document.body.setAttribute("data-gh-theme", theme);
-    switchInput.checked = theme === "dark";
+    if (switchInput) {
+      switchInput.checked = theme === "dark";
+    }
   }
 
-  // Get system configuration and apply the corresponding theme
+  // Get system preference
   const prefersDarkScheme = window.matchMedia(
     "(prefers-color-scheme: dark)"
   ).matches;
-
-  // Check if there's a saved theme and if the system preference is dark
   const savedTheme = localStorage.getItem("theme");
-  if (savedTheme) {
-    applyTheme(savedTheme);
-  } else if (prefersDarkScheme) {
-    applyTheme("dark");
-  } else {
-    applyTheme("light");
+
+  // Apply saved theme, else follow system preference
+  const theme = savedTheme || (prefersDarkScheme ? "dark" : "light");
+  applyTheme(theme);
+
+  // Change theme when the switch is toggled
+  if (switchInput) {
+    switchInput.addEventListener("change", function () {
+      const newTheme = switchInput.checked ? "dark" : "light";
+      applyTheme(newTheme);
+      localStorage.setItem("theme", newTheme);
+    });
   }
 
-  // Change the theme when the switch toggles
-  switchInput.addEventListener("change", function () {
-    const theme = switchInput.checked ? "dark" : "light";
-    applyTheme(theme);
-    localStorage.setItem("theme", theme); // Save the setting to localStorage
-  });
-
-  // Listen for changes in system configuration
+  // Listen for system preference changes
   window
     .matchMedia("(prefers-color-scheme: dark)")
     .addEventListener("change", function (e) {
       const newTheme = e.matches ? "dark" : "light";
       applyTheme(newTheme);
-      localStorage.setItem("theme", newTheme); // Save the setting to localStorage
+      localStorage.setItem("theme", newTheme);
     });
 });
